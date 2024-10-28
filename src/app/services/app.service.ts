d956bbc86e1e926ad28ade4207d8fc705d666a60
Ananta Poudel <anantapoudel580@gmail.com>
2024-10-28 17:46:19 -0400

    Api key change

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class AppService {
   // private apiKey: string = '084227eac0msh826fc15a269fbd4p16e1cfjsn152e26c3dc83';
-  private apiKey: string = '0e1753dfb2msh55d3e2acd288959p1a1ddcjsn27a8edd4f075';
+  private apiKey: string = 'a01285857bmshb9cdc2c8a3ca2fbp19a15fjsn60b801d1d602';
   private host: string = 'jsearch.p.rapidapi.com';
 
   private baseUrl: string = 'https://jsearch.p.rapidapi.com';
